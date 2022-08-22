dca1bf16be39cb25a2a5fff60b19ba08dec0f0a1
Victor Bondarenko <nirasimha@gmail.com>
2022-08-22 08:04:51 -0400

    add start async func with tryCatch/ move app.listen to try

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-require('./db/connect');
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
 
 
 // middleware
@@ -26,4 +26,13 @@ app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`Server listening on ${port}...`));
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, console.log(`Server listening on ${port}...`));
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+start();
