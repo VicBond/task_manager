a4aeaea353233de1cda36a2681ca35925d90bf21
Victor Bondarenko <nirasimha@gmail.com>
2022-08-24 08:35:25 -0400

    add module exports TaskSchema model

--- models/Task.js
+++ models/Task.js
@@ -5,4 +5,7 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
   name: String,
   complete: Boolean
-})
+});
+
+
+module.exports = mongoose.model('Task', TaskSchema);
