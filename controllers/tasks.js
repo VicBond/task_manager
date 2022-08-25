f1152d2ca6cae41d2dea45a4224f1f0b00917720
Victor Bondarenko <nirasimha@gmail.com>
2022-08-25 10:00:23 -0400

    add trycatch in createTask method

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,16 +3,28 @@ const Task = require('../models/Task');
 const getAllTasks = (req, res) => {
   res.send('all items');
 };
+
+
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({msg: error});
+  }
 };
+
+
 const getTask = (req, res) => {
   res.json({id:req.params.id});
 };
+
+
 const updateTask = (req, res) => {
   res.send('update task');
 };
+
+
 const deleteTask = (req, res) => {
   res.send('delete task');
 };
